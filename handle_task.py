69b0cca483196e704641ccf1174060a9dfe8761e
mcko <jkw552403@gmail.com>
2021-01-27 22:40:54 +0900

    implement toggl tracking

--- handle_task.py
+++ handle_task.py
@@ -2,7 +2,13 @@
 import sys
 
 import click
-from common import create_todoist_sync_client, get_todoist_state
+from common import (
+    create_todoist_sync_client,
+    create_toggl_client,
+    get_todoist_state,
+    get_toggl_project_map,
+)
+from setting import TODOIST_API_TOKEN, TOGGL_API_TOKEN
 from workflow import Workflow3
 from workflow.notify import notify
 
@@ -17,17 +23,34 @@ wf = None
 def cli(task_id, complete, track):
     if track and complete:
         raise ValueError("Only one of track and complete can be true.")
-    sync_client = create_todoist_sync_client()
+    sync_client = create_todoist_sync_client(wf.settings[TODOIST_API_TOKEN])
     todoist_state = get_todoist_state(wf, sync_client)
     item = next(item for item in todoist_state["items"] if item["id"] == task_id)
     if track:
         # TODO
-        # 1. Get item name and use it for time entry description
-        # 2. Get project name and try to map it to Toggl project
-        #    If project with the same name exists, set pid to this project ID
-        #    Else, pid = None
+        # Get project name and try to map it to Toggl project
+        todoist_project_name = next(
+            (
+                p["name"]
+                for p in todoist_state["projects"]
+                if p["id"] == item["project_id"]
+            ),
+            None,
+        )
+        toggl_client = create_toggl_client(wf.settings[TOGGL_API_TOKEN])
+        toggl_project_map = get_toggl_project_map(wf, toggl_client)
+        # If project with the same name exists, set pid to this project ID
+        toggl_project_id = toggl_project_map.get(todoist_project_name)
+        # Start a new time entry
+        toggl_client.start_time_entry(item["content"], toggl_project_id)
         # Update notify message to show tracking project
-        notify(u"Start tracking", item[u"content"])
+        notify(
+            u"Start tracking",
+            "{} ({})".format(
+                item["content"],
+                todoist_project_name if toggl_project_id else "No Project",
+            ),
+        )
     if complete:
         item.complete()
         sync_client.commit()
